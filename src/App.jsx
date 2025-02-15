d80f65b69f0e8d36a1ee76bede3d36647d11dab1
ShuvamKandel <shuvamkandel007@gmail.com>
2025-02-15 18:23:35 +0545

    Fixed gap in Image section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import NavBar from "./Components/Navigation Bar/NavBar";
-import ImageSection from "./Components/Image Section/ImageSection";
-import Breaker from "./Components/Trending Products/Breaker";
-import GridComponent from "./Components/Cool Articles/GridArticles";
+import NavBar from "./Components/NavigationBar/NavBar";
+import ImageSection from "./Components/HomePage/Image Section/ImageSection";
+import Breaker from "./Components/HomePage/Trending Products/Breaker";
+import GridComponent from "./Components/HomePage/Cool Articles/GridArticles"
 import Footer from "./Components/Footer/Footer";
 import './app.scss';
 
